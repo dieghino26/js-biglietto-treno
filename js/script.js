134eaf4b4d6590cdd6074f4dc022353d296c0e12
dieghino26 <brotzadiego@gmail.com>
2023-12-20 16:14:26 +0100

    added price and discount

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+const total = document.getElementById("total")
+
+
+
 //chiedo info all'utente
 const kms = parseInt(prompt("quanti km devi percorrere?").trim())
 const age = parseInt(prompt("quanti anni hai?"))
+
+//calcolo del prezzo
+let price = kms * 0.21;
+
+if (age < 18) {
+    price = (price * 0.8)
+} else if (age > 65) {
+    price = (price * 0.6)
+}
+
+
+
+total.innerText = "Il biglietto costa: €" + price.toFixed(2) 
